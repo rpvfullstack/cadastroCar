7343d9092ee5e13ac5509e2887f464794059c46d
rpvfullstack <164066743+rpvfullstack@users.noreply.github.com>
2024-07-29 18:17:59 -0300

    Update2 index.tsx

--- src/components/Logo/index.tsx
+++ src/components/Logo/index.tsx
@@ -6,8 +6,8 @@ export const Logo = () => {
 
     return (
         <div className="logo" onClick={() => navigate('/')}>
-            <div className="mainLogo"><span>CAR</span>DASTRO</div>
-            <div className="powered">powered by B7Web</div>
+            <div className="mainLogo"><span>CADASTRO</span>CARS</div>
+            <div className="powered">powered by RPV</div>
         </div>
     );
 }
